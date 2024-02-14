2ad0c55328a828056894742bd25e09133ad25354
Josh Rarick <rarick.joshua.e@gmail.com>
2024-02-13 23:46:03 -0600

    initial commit

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,9 +1,40 @@
 const express = require('express')
 const port = 8080
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const csv = require('csv-parser')
+const fs = require('fs')
+const cors = require('cors')
 
 const app = express()
 
 app.listen(port, () => {
 	console.log('-----------------------------------')
 	console.log(`SERVER STARTED AND LISTENING ON PORT ${port}`)
+})
+
+app.post('/api/upload', cors(), upload.single('file'), (req, res) => {
+	const results = []
+	let calculatedResults = []
+
+	function convertToNumber(str) {
+		const match = str.match(/\d+(\.\d+)?/);
+		return match ? Number(match[0]) : null;
+	}
+
+	fs.createReadStream(req.file.path)
+		.pipe(csv())
+		.on('data', (data) => results.push(data))
+		.on('end', () => {
+			calculatedResults = JSON.parse(JSON.stringify(results))
+
+			calculatedResults.forEach(result => {
+				result.Density = convertToNumber(result.Mass) / convertToNumber(result.Volume)
+
+				delete result.Mass
+				delete result.Volume
+			})
+
+			res.json({ original: results, calculated: calculatedResults})
+		})
 })
--- src/frontend/Dashboard.jsx
+++ src/frontend/Dashboard.jsx
@@ -1,7 +1,59 @@
+import { useState } from 'react'
+import '../css/dashboard.css'
+
 export default function Dashboard() {
+	const [file, setFile] = useState(null)
+	const [convertedData, setConvertedData] = useState([])
+
+	const uploadFile = async () => {
+		const formData = new FormData()
+		formData.append('file', file)
+
+		await fetch('http://localhost:8080/api/upload', {
+			method: 'POST',
+			body: formData
+		})
+			.then((res) => res.json())
+			.then((data) => setConvertedData(data))
+			.catch((err) => console.error(err))
+	}
+
 	return (
-		<main>
-			<h1>DASHBOARD PAGE - PLACEHOLDER</h1>
+		<main className='main-container'>
+			<div className='card'>
+				<h1 className='card-header'>Dashboard Page</h1>
+				<h2 className='card-header'>Upload a .csv file to convert it to JSON</h2>
+				<form
+					className='card-body'
+					onSubmit={(e) => {
+						e.preventDefault()
+						uploadFile(file)
+					}}
+				>
+					<input
+						className='input-file'
+						type='file'
+						accept='.csv'
+						onChange={(e) => setFile(e.target.files[0])}
+						required
+					/>
+					<button type='submit' className='button'>
+						Convert File to JSON
+					</button>
+				</form>
+			</div>
+			{convertedData.calculated && (
+				<div className='json-columns'>
+					<div className='json'>
+						<h3 className='json-heading'>Original Data</h3>
+						<pre>{JSON.stringify(convertedData.original, null, 4)}</pre>
+					</div>
+					<div className='json'>
+						<h3 className='json-heading'>Calculated Data</h3>
+						<pre>{JSON.stringify(convertedData.calculated, null, 4)}</pre>
+					</div>
+				</div>
+			)}
 		</main>
 	)
 }
